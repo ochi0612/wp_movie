d6025afcd93aa96eb87a637f7137ba490e3274ea
mediaplex-ochi <k-ochi@mediaplex.co.jp>
2020-01-29 19:00:18 +0900

    ジャンル追加

--- wp-content/themes/movie/lib/js/action.js
+++ wp-content/themes/movie/lib/js/action.js
@@ -6,39 +6,5 @@ function menuAction() {
     var nav = document.querySelector('.head-nav');
 
     listBtn.classList.toggle('active');
-    nav.classList.toggle('open');
-    nav.classList.toggle('fadein');
-}
-
-
-function fadeIn(node, duration) {
-    // display: noneでないときは何もしない
-    if (getComputedStyle(node).display !== 'none') {
-        return;
-    }
-
-    // style属性にdisplay: noneが設定されていたとき
-    if (node.style.display === 'none') {
-        node.style.display = '';
-    } else {
-        node.style.display = 'block';
-    }
-    node.style.opacity = 0;
-
-    var start = performance.now();
-
-    requestAnimationFrame(function tick(timestamp) {
-        // イージング計算式（linear）
-        var easing = (timestamp - start) / 2000;
-
-        // opacityが1を超えないように
-        node.style.opacity = Math.min(easing, 1);
-
-        // opacityが1より小さいとき
-        if (easing < 1) {
-            requestAnimationFrame(tick);
-        } else {
-            node.style.opacity = '';
-        }
-    });
+    nav.classList.toggle('close');
 }
